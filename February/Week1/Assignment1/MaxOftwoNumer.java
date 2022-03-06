2bb0cd6fb42850751a45e6e56a006377bf76fe9a
Mohd Used <91274057+Usaid02@users.noreply.github.com>
2022-03-06 17:42:42 +0530

    Create MaxOftwoNumer.java

---
+++ February/Week1/Assignment1/MaxOftwoNumer.java
@@ -0,0 +1,13 @@
+package febweekfirst;
+//                                Number is maximum
+public class MaxOfTwoNumber {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 4;
+        if(a>b){
+            System.out.println("a is maximum");
+        }else{
+            System.out.println("b is minimum");
+        }
+    }
+}
